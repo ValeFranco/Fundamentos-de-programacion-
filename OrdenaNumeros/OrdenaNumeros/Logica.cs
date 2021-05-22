5d3ea8f3c1985d1363fd51e36d4f64de88773c6a
Sofia Diaz Zapata <sofi.diaz.z@hotmail.com>
2021-05-22 12:58:14 -0500

    Primer cambio de Logica

---
+++ OrdenaNumeros/OrdenaNumeros/Logica.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OrdenaNumeros
+{
+    class Logica
+    {//Atributos propios del juego
+        private int[,] matrizValores;
+        private int posicionFila, posicionColumna;
+
+
+        public int PosicionFila
+        {
+            get
+            {
+                return posicionFila;
+            }
+            set
+            {
+                if (value >= 0 && value <= 17)
+                {
+                    posicionFila = value;
+                }
+            }
+        }
+
+        public int PosicionColumna
+        {
+            get
+            {
+                return posicionColumna;
+            }
+            set
+            {
+                if (value >= 0 && value <= 17)
+                {
+                    posicionColumna = value;
+                }
+            }
+        }
+    }
+}
